601196221f96215ed117bad2c41e3abdf501c5ce
ROOter <ralph@rfactory.org>
2022-01-04 09:55:09 -0800

    time zone
    time zone

--- package/rooter/ext-rooter-basic/files/usr/lib/rooter/timezone.sh
+++ package/rooter/ext-rooter-basic/files/usr/lib/rooter/timezone.sh
@@ -6,13 +6,14 @@ wget --no-check-certificate -O /tmp/timez http://ip-api.com/json > /dev/null 2>&
 OX=$(cat /tmp/timez)
 
 TZ=$(echo $OX" " | tr -d '"' | grep -o 'timezone:[^;]*' | tr ":" ",")
-ZONEN=$(echo $TZ | cut -d, -f2)
+ZONEN=$(echo "$TZ" | cut -d, -f2)
 
 /usr/lib/rooter/tzone.lua "$ZONEN"
 source /tmp/tzone
 
-	uci set system.@system[-1].timezone="$ZNAME" 
-	uci set system.@system[-1].zonename="$ZONEN" 
-	uci commit system
-	#/etc/init.d/system restart
+uci set system.@system[-1].timezone="$ZNAME" 
+uci set system.@system[-1].zonename="$ZONEN" 
+uci commit system
+/etc/init.d/system restart
+
 
