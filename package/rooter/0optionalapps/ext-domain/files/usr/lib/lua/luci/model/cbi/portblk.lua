5eba5f5b8a8a1d305cf4a9f8ddad302abbf7e5a2
ROOter <ralph@rfactory.org>
2022-02-03 11:25:43 -0800

    MBIM gateway
    MBIM gateway

--- package/rooter/0optionalapps/ext-domain/files/usr/lib/lua/luci/model/cbi/portblk.lua
+++ package/rooter/0optionalapps/ext-domain/files/usr/lib/lua/luci/model/cbi/portblk.lua
@@ -5,17 +5,17 @@ local zones = require "luci.sys.zoneinfo"
 local fs    = require "nixio.fs"
 local conf  = require "luci.config"
 
-m = Map("filter", translate("Domain Filter"), translate("Block traffic to specified URLs"))
+m = Map("blockport", translate("Port Blocking"), translate("Block traffic using specific ports"))
 
 m.on_after_save = function(self)
-	luci.sys.call("/usr/lib/domain/filter.sh &")
+	luci.sys.call("/usr/lib/blockport/blockport.sh &")
 end
 
-s = m:section(TypedSection, "filter", translate("Block List"))
+s = m:section(TypedSection, "port", translate("Block Port List"))
 s.anonymous = true
 s.addremove = false
 
-s:option(DynamicList, "ipset", translate("Domain URLs"))
+s:option(DynamicList, "block", translate("Ports"))
 
 
 return m
